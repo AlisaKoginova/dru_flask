47b2cecfa6b68419d2211c4f39b3be113d5fe810
alisa <alisa.kog12@gmail.com>
2020-10-05 16:51:16 +0300

    Fix LinearRegression.pickle

--- settings/constants.py
+++ settings/constants.py
@@ -1,7 +1,7 @@
 import os
 
-DATA_FOLDER = 'app/data'
-MODELS_FOLER = 'app/models'
+DATA_FOLDER = 'data'
+MODELS_FOLER = 'models'
 TRAIN_CSV = os.path.join(DATA_FOLDER, 'train.csv')
 VAL_CSV = os.path.join(DATA_FOLDER, 'val.csv')
 SAVED_ESTIMATOR = os.path.join(MODELS_FOLER, 'LinearRegression.pickle')
